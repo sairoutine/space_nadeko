289a3429c6753bfcc717d8424b633382ba2c7f97
sairoutine <sairoutine@gmail.com>
2016-07-13 21:40:28 +0900

    refactor

--- 1/js/index.js
+++ 1/js/index.js
@@ -4,88 +4,104 @@
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
 $(function () {
-	var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera(45 , WIDTH / HEIGHT , 0.1, 1000);
-
 	var renderer = new THREE.WebGLRenderer();
 
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.autoClear = false ;
 
-	var planetGeometry = new THREE.SphereGeometry(4,20,20);
+	var planet = (function() {
+		var planetGeometry = new THREE.SphereGeometry(4,20,20);
 
-	//Load the planet textures
-	var texture = THREE.ImageUtils.loadTexture("./img/planet-512.jpg");
-	var normalmap = THREE.ImageUtils.loadTexture("./img/normal-map-512.jpg");
-	var specmap = THREE.ImageUtils.loadTexture("./img/water-map-512.jpg");
+		//Load the planet textures
+		var texture = THREE.ImageUtils.loadTexture("./img/planet-512.jpg");
+		var normalmap = THREE.ImageUtils.loadTexture("./img/normal-map-512.jpg");
+		var specmap = THREE.ImageUtils.loadTexture("./img/water-map-512.jpg");
 
-	var planetMaterial = new THREE.MeshPhongMaterial();
-	planetMaterial.map = texture;
+		var planetMaterial = new THREE.MeshPhongMaterial();
+		planetMaterial.map = texture;
 
-	planetMaterial.specularMap = specmap;
-	planetMaterial.specular = new THREE.Color( 0xff0000 );
-	planetMaterial.shininess = 1;
+		planetMaterial.specularMap = specmap;
+		planetMaterial.specular = new THREE.Color( 0xff0000 );
+		planetMaterial.shininess = 1;
 
-	planetMaterial.normalMap = normalmap;
-	planetMaterial.normalScale.set(-0.3,-0.3);
+		planetMaterial.normalMap = normalmap;
+		planetMaterial.normalScale.set(-0.3,-0.3);
 
-	var planet = new THREE.Mesh(planetGeometry, planetMaterial);
+		var planet = new THREE.Mesh(planetGeometry, planetMaterial);
 
-	//here we allow the texture/normal/specular maps to wrap
-	planet.material.map.wrapS = THREE.RepeatWrapping;
-	planet.material.map.wrapT = THREE.RepeatWrapping;
-	planet.material.normalMap.wrapS = THREE.RepeatWrapping;
-	planet.material.normalMap.wrapT = THREE.RepeatWrapping;
-	planet.material.specularMap.wrapS = THREE.RepeatWrapping;
-	planet.material.specularMap.wrapT = THREE.RepeatWrapping;
+		//here we allow the texture/normal/specular maps to wrap
+		planet.material.map.wrapS = THREE.RepeatWrapping;
+		planet.material.map.wrapT = THREE.RepeatWrapping;
+		planet.material.normalMap.wrapS = THREE.RepeatWrapping;
+		planet.material.normalMap.wrapT = THREE.RepeatWrapping;
+		planet.material.specularMap.wrapS = THREE.RepeatWrapping;
+		planet.material.specularMap.wrapT = THREE.RepeatWrapping;
 
-	//here we repeat the texture/normal/specular maps twice along X
-	planet.material.map.repeat.set( 2, 1);
-	planet.material.normalMap.repeat.set( 2, 1);
-	planet.material.specularMap.repeat.set( 2, 1);
+		//here we repeat the texture/normal/specular maps twice along X
+		planet.material.map.repeat.set( 2, 1);
+		planet.material.normalMap.repeat.set( 2, 1);
+		planet.material.specularMap.repeat.set( 2, 1);
 
-	planet.position.x = 0;
-	planet.position.y = 0;
-	planet.position.z = 0;
+		planet.position.x = 0;
+		planet.position.y = 0;
+		planet.position.z = 0;
 
-	scene.add(planet);
+		return planet;
+	})();
 
-	//Space background is a large sphere
-	var spacetex = THREE.ImageUtils.loadTexture("./img/space.jpg");
-	var spacesphereGeo = new THREE.SphereGeometry(20,20,20);
-	var spacesphereMat = new THREE.MeshPhongMaterial();
-	spacesphereMat.map = spacetex;
+	var spacesphere = (function() {
+		//Space background is a large sphere
+		var spacetex = THREE.ImageUtils.loadTexture("./img/space.jpg");
+		var spacesphereGeo = new THREE.SphereGeometry(20,20,20);
+		var spacesphereMat = new THREE.MeshPhongMaterial();
+		spacesphereMat.map = spacetex;
 
-	var spacesphere = new THREE.Mesh(spacesphereGeo,spacesphereMat);
+		var spacesphere = new THREE.Mesh(spacesphereGeo,spacesphereMat);
 
-	//spacesphere needs to be double sided as the camera is within the spacesphere
-	spacesphere.material.side = THREE.DoubleSide;
+		//spacesphere needs to be double sided as the camera is within the spacesphere
+		spacesphere.material.side = THREE.DoubleSide;
 
-	spacesphere.material.map.wrapS = THREE.RepeatWrapping;
-	spacesphere.material.map.wrapT = THREE.RepeatWrapping;
-	spacesphere.material.map.repeat.set( 5, 3);
+		spacesphere.material.map.wrapS = THREE.RepeatWrapping;
+		spacesphere.material.map.wrapT = THREE.RepeatWrapping;
+		spacesphere.material.map.repeat.set( 5, 3);
+		return spacesphere;
+	})();
 
-	scene.add(spacesphere);
+	// 3Dシーン
+	var scene = (function(){
+		var scene = new THREE.Scene();
+		scene.add(planet);
+		scene.add(spacesphere);
 
-	//position camera
-	camera.position.x = 0;
-	camera.position.y = 0;
-	camera.position.z = -15;
-	camera.lookAt(scene.position);
+		//create two spotlights to illuminate the scene
+		var spotLight = new THREE.SpotLight( 0xffffff );
+		spotLight.position.set( -40, 60, -10 );
+		spotLight.intensity = 2;
+		scene.add( spotLight );
 
-	//create two spotlights to illuminate the scene
-	var spotLight = new THREE.SpotLight( 0xffffff );
-	spotLight.position.set( -40, 60, -10 );
-	spotLight.intensity = 2;
-	scene.add( spotLight );
+		var spotLight2 = new THREE.SpotLight( 0x5192e9 );
+		spotLight2.position.set( 40, -60, 30 );
+		spotLight2.intensity = 1.5;
+		scene.add( spotLight2 );
 
-	var spotLight2 = new THREE.SpotLight( 0x5192e9 );
-	spotLight2.position.set( 40, -60, 30 );
-	spotLight2.intensity = 1.5;
-	scene.add( spotLight2 );
+		return scene;
+	})();
 
+	// 3D カメラ
+	var camera = (function(){
+		var camera = new THREE.PerspectiveCamera(45 , WIDTH / HEIGHT , 0.1, 1000);
+		camera.position.x = 0;
+		camera.position.y = 0;
+		camera.position.z = -15;
+		camera.lookAt(scene.position);
+
+		return camera;
+	})();
+
+	// 2Dカメラ
 	var camera2d = new THREE.OrthographicCamera(45 , WIDTH / HEIGHT , 0.1, 1000);
 
+	// 2Dシーン
 	var scene2d = create2DScene();
 
 	$("#WebGL-output"). append(renderer.domElement);
@@ -93,12 +109,13 @@ $(function () {
 	render();
 	//render loop
 	function render() {
-		requestAnimationFrame(render);
 		//rotate planet and spacesphere
 		planet.rotation.y += 0.002;
 		spacesphere.rotation.y += 0.001;
 		renderer.render(scene, camera);
 		renderer.render(scene2d, camera2d);
+
+		requestAnimationFrame(render);
 	}
 
 	function create2DScene() {
@@ -116,7 +133,4 @@ $(function () {
 
 		return scene2d;
 	}
-
-
-
 });
